1b5d65b235625d3f08999a932d3e4dc951dcfa19
Diogo Carneiro <diogoascarneiro@hotmail.com>
2024-09-03 18:14:13 +0100

    completed logic page

--- src/routes/svelte-basics/logic/+page.svelte
+++ src/routes/svelte-basics/logic/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Card from '$lib/components/ui/card/card.svelte';
 	import CodeBlock from '$lib/components/ui/code-block/code-block.svelte';
 	import { Code } from 'lucide-svelte';
 </script>
@@ -58,3 +60,58 @@
 </ul>
 
 <h2>Each blocks</h2>
+
+<p>
+	An each block is used to iterate over an array and render content for each item in the array,
+	similar to how you'd map over an array in React.
+</p>
+
+<CodeBlock>
+	{`{#each array as item}
+	<p>{item}</p>
+{/each}`}
+</CodeBlock>
+
+<p>Also like mapping in react, you can get the iteration number as the second param like so:</p>
+
+<CodeBlock>
+	{`{#each array as item, i}
+	<p>{i}: {item}</p>
+{/each}`}
+</CodeBlock>
+
+<h2>Keyed each blocks</h2>
+
+<p>
+	By default, Svelte uses the index of the array as the key for each item. This can lead to
+	performance issues if the array is reordered. To avoid this, you can specify a key for each item
+	like so:
+</p>
+
+<CodeBlock>
+	{`{#each array as item (item.id)}
+	<p>{item}</p>
+{/each}`}
+</CodeBlock>
+
+<h2>Await blocks</h2>
+
+<p>
+	An await block is used to wait for a promise to resolve before rendering content. This is useful
+	for fetching data from an API or performing other asynchronous operations.
+</p>
+
+<CodeBlock>
+	{`{#await promise}
+	<p>Loading...</p>
+{:then data}
+	<p>{data}</p>
+{:catch error}
+	<p>{error}</p>
+{/await}`}
+</CodeBlock>
+
+<p>
+	The first block ("Loading...") is displayed while the promise is resolving, which is a nice and
+	convenient way to add placeholders directly in the HTML.
+</p>
