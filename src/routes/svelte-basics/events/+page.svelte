b4c71e70ffdd82ba813dea6ef001262a49d26df5
Joao Nacho <joao.nacho@studiographene.com>
2024-09-11 17:59:05 +0100

    event modifiers, inline handlers, keyboard events

--- src/routes/svelte-basics/events/+page.svelte
+++ src/routes/svelte-basics/events/+page.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
 	import CodeBlock from '$lib/components/ui/code-block/code-block.svelte';
+	const mouseEventsList = [
+		{ event: 'on:mouseover', description: 'Fires when the mouse pointer moves over an element.' },
+		{ event: 'on:mouseout', description: 'Fires when the mouse pointer moves out of an element.' },
+		{ event: 'on:mouseenter', description: 'Fires when the mouse pointer enters an element.' },
+		{ event: 'on:mouseleave', description: 'Fires when the mouse pointer leaves an element.' }
+	];
+
+	const keyboardEventsList = [
+		{ event: 'on:keydown', description: 'Fires when a key is pressed down.' },
+		{ event: 'on:keyup', description: 'Fires when a key is released.' },
+		{ event: 'on:keypress', description: 'Fires when a key is pressed and released.' }
+	];
+
+	const eventModifiersList = [
+		{ modifier: '|preventDefault', description: 'Prevents the default behavior of the event.' },
+		{ modifier: '|stopPropagation', description: 'Stops the event from propagating.' },
+		{
+			modifier: '|self',
+			description: 'Only triggers the event if the event was dispatched from the element itself.'
+		},
+		{ modifier: '|once', description: 'Only triggers the event once.' },
+		{ modifier: '|capture', description: 'Use capture phase instead of bubbling phase.' },
+		{ modifier: '|passive', description: 'Use passive event listener.' },
+		{ modifier: '|nonpassive', description: 'Use non-passive event listener.' },
+		{
+			modifier: '|trusted',
+			description: 'Only trigger the event if it was dispatched by a user action.'
+		}
+	];
 </script>
 
 <h1>Events</h1>
@@ -11,7 +40,6 @@
 </p>
 
 <h2>Click events</h2>
-
 <p>
 	The most common event in Svelte is the click event. It's used to listen for clicks on an element.
 </p>
@@ -26,15 +54,50 @@
 </p>
 
 <h2>Mouse events</h2>
+<ul>
+	{#each mouseEventsList as { event, description }}
+		<li><code>{event}</code> - {description}</li>
+	{/each}
+</ul>
 
+<h2>Keyboard events</h2>
+<ul>
+	{#each keyboardEventsList as { event, description }}
+		<li><code>{event}</code> - {description}</li>
+	{/each}
+</ul>
+
+<h2>Inline handlers</h2>
 <p>
-	Svelte also provides several mouse events that can be used to listen for mouse interactions. Some
-	of the most common mouse events are:
+	Svelte also allows us to declare events handlers inline. In some other JS frameworks, you may see
+	recommendations to avoid inline event handlers due to performance issues, particularly inside
+	loops. In Svelte, since the compiler will always do the right thing, this advice really
+	<span class="underline">doesn't make a difference</span>. You can use inline event handlers like
+	this:
 </p>
 
+<CodeBlock>
+	{`<button on:mouseover={() => {console.log('Mouse is hovering me')}}>Hover me</button>`}
+</CodeBlock>
+
+<h2>Event modifiers</h2>
+<p>
+	Event modifiers are used to modify the behavior of an event. They can be used to prevent the
+	default behavior of an event, stop the event from propagating, or pass additional data to the
+	event handler. In the following example, we use the <code>|preventDefault</code> modifier to prevent
+	the default behavior of the click event:
+</p>
+<CodeBlock>
+	{`<button on:click|preventDefault={handleSubmit}>Submit</button>`}
+</CodeBlock>
+
 <ul>
-	<li><code>on:mouseover</code>: Fires when the mouse pointer moves over an element.</li>
-	<li><code>on:mouseout</code>: Fires when the mouse pointer moves out of an element.</li>
-	<li><code>on:mouseenter</code>: Fires when the mouse pointer enters an element.</li>
-	<li><code>on:mouseleave</code>: Fires when the mouse pointer leaves an element.</li>
+	{#each eventModifiersList as { modifier, description }}
+		<li><code>{modifier}</code> - {description}</li>
+	{/each}
 </ul>
+
+<p>
+	You can even chain modifiers together. For example:
+	<code>on:click|preventDefault|stopPropagation=&#123;handleClick&#125;</code>
+</p>
