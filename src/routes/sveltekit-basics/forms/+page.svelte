af00dc999ce02b2658dca5b474a29ab54d8583c9
Diogo Carneiro <diogoascarneiro@hotmail.com>
2024-09-12 17:49:02 +0100

    feat: added shared-modules page, began creating forms page

---
+++ src/routes/sveltekit-basics/forms/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Forms</h1>
+
+<p>
+	Forms are a fundamental part of web development. They allow users to input data and submit it to
+	the server. In this section, we'll cover how to work with forms to submit data in SvelteKit.
+</p>
+
+<h2>Example form</h2>
+
+<form method="POST"></form>
---
+++ src/routes/sveltekit-basics/shared-modules/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import CodeBlock from '$lib/components/ui/code-block/code-block.svelte';
+</script>
+
+<h1>Sharing modules</h1>
+
+<p>
+	Shared modules are a way to share code between different parts of your SvelteKit application. They
+	can be used to share functions, variables, and other code that you want to reuse in multiple
+	components or pages.
+</p>
+
+<p>
+	To make code available to the entirety of your application, simply place it in a file in the <code
+		>src/lib</code
+	>
+	directory. You can then import this code into any component or page using the <code>$lib</code> alias.
+	For example:
+</p>
+
+<CodeBlock>
+	{`<script>
+        import CodeBlock from "$lib/components/ui/code-block/code-block.svelte";
+    
+    <\/script>`}
+</CodeBlock>
