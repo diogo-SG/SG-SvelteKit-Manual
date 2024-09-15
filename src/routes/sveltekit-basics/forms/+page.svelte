2aa6e73d397cf995a0e06aea89bae92f4f25c756
Diogo Carneiro <diogoascarneiro@hotmail.com>
2024-09-15 16:35:54 +0100

    feat: added form submission and handling example

--- src/routes/sveltekit-basics/forms/+page.svelte
+++ src/routes/sveltekit-basics/forms/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import CodeBlock from '$lib/components/ui/code-block/code-block.svelte';
+	import { Code } from 'lucide-svelte';
+
+	export let form;
+	$: console.log(form);
+</script>
+
 <h1>Forms</h1>
 
 <p>
@@ -7,4 +16,104 @@
 
 <h2>Example form</h2>
 
-<form method="POST"></form>
+<form method="POST">
+	<label> Search for a movie: </label>
+	<input name="movieTitle" type="text" placeholder="Enter a movie title" />
+	<Button type="submit">Search</Button>
+</form>
+
+{#if form?.searchResults}
+	<h2>Search results</h2>
+	<div class="results">
+		{#each form.searchResults as result}
+			<div class="search-result">
+				<p class="movie-title">{result.title}</p>
+				<p><b>Year:</b> {result.year}</p>
+				<p><b>Rating:</b> {result.rating}</p>
+				<p><b>Plot:</b> {result.plot}</p>
+			</div>
+		{/each}
+	</div>
+{/if}
+
+<h2>How it works</h2>
+
+<p>
+	A Svelte form is just like a regular HTML form. When we define its method as POST, it'll make a
+	post request to the current page. We can then create a server-side <code>action</code> to handle the
+	form submission. Here's the code for the form above:
+</p>
+
+<CodeBlock>
+	{`
+	export const actions = {
+	default: async ({ request, fetch }) => {
+		const formData = await request.formData();
+		const movieTitle = formData.get('movieTitle');
+
+		return fetch(\`https://freetestapi.com/api/v1/movies?search=\${movieTitle}\`)
+			.then((response) => response.json())
+			.then((data) => {
+				return {
+					searchResults: data
+				};
+			});
+	}
+};
+	`}
+</CodeBlock>
+
+<style>
+	form {
+		display: flex;
+		border: 1px solid #ccc;
+		padding: 1rem;
+		flex-direction: column;
+	}
+
+	label {
+		margin-bottom: 0.5rem;
+		font-weight: bold;
+	}
+
+	input {
+		padding: 0.5rem;
+		margin-bottom: 1rem;
+	}
+
+	button {
+		padding: 0.5rem;
+	}
+
+	.results {
+		margin-top: 1rem;
+		display: flex;
+		justify-content: left;
+		flex-wrap: wrap;
+	}
+
+	.movie-title {
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+
+	.search-result {
+		margin-top: 1rem;
+		border: 1px solid #ccc;
+		padding: 1rem;
+		max-width: 24%;
+		margin-right: 1%;
+	}
+
+	.search-result p {
+		margin-bottom: 0.5rem;
+	}
+
+	.search-result p b {
+		font-weight: bold;
+	}
+
+	.search-result p:last-child {
+		margin-bottom: 0;
+	}
+</style>
