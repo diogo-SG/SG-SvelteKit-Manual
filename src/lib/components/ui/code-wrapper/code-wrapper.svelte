388e52caa9a8e1e3bbf337e2a59ebc7ec000f265
Joao Nacho <joao.nacho@studiographene.com>
2024-09-16 12:21:40 +0100

    constant file for sveltebasics stores, component for code block, updtd stores tutorial page

---
+++ src/lib/components/ui/code-wrapper/code-wrapper.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import 'highlight.js/styles/base16/dracula.css';
+	import { CodeBlock } from 'svhighlight';
+
+	export let headerText: string;
+	export let code: string;
+</script>
+
+<CodeBlock language="svelte" {headerText} {code} />
