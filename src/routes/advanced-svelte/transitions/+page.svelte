43c4fed65a78cff5c93c6412732620d543d2a804
Joao Nacho <joao.nacho@studiographene.com>
2024-09-17 16:32:52 +0100

    fade transition

--- src/routes/advanced-svelte/transitions/+page.svelte
+++ src/routes/advanced-svelte/transitions/+page.svelte
@@ -1,4 +1,21 @@
 <script lang="ts">
+	import CodeWrapper from '$lib/components/ui/code-wrapper/code-wrapper.svelte';
+	import { CODE } from './constants';
+
+	const fileTitle = 'App.svelte';
 </script>
 
 <h1>Transitions</h1>
+
+<p>
+	Transitions in Svelte are a way to animate elements when they are added or removed from the DOM.
+	They are created using the <code>transition</code> directive. Let's import the <code>fade</code>
+	function from <code>svelte/transition</code> into our <code>App.svelte</code> file and create a small
+	example:
+</p>
+
+<CodeWrapper headerText={fileTitle} code={CODE.TRANSITION_FADE} />
+
+<p>
+	The <code>fade</code> function creates a fade-in and fade-out effect and its very easy to use.
+</p>
