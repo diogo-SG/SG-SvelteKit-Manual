5f64fb65a04319bb90e41e0af5c7278e0e410a07
diogo-SG <diogo.carneiro@studiographene.com>
2024-09-20 18:02:33 +0100

    added extra info page stub

---
+++ src/routes/extra-info/+page.svelte
@@ -0,0 +1,39 @@
+<h1>Extra info and resources</h1>
+
+<h2>Advantages and disadvantages of Svelte / Sveltekit</h2>
+
+<p>
+	Svelte is a great choice for building web applications, but it's not perfect. Here are
+	some of the pros and cons of using Svelte:
+</p>
+
+<!-- Todo review -->
+<h3>Advantages</h3>
+<ul>
+	<li>Easy to learn</li>
+	<li>Fast and lightweight</li>
+	<li>Great developer experience</li>
+	<li>Great performance</li>
+	<li>Great for building web applications</li>
+</ul>
+
+<h3>Disadvantages</h3>
+<ul>
+	<li>Not as popular as React or Vue</li>
+	<li>Smaller ecosystem</li>
+	<li>Less community support</li>
+	<li>Less third-party libraries</li>
+</ul>
+
+<h2>Official resources</h2>
+<ul>
+	<li><a href="https://svelte.dev/">Svelte</a></li>
+	<li><a href="https://kit.svelte.dev/">SvelteKit</a></li>
+</ul>
+
+<h2>Component libraries</h2>
+<!-- todo improve -->
+<ul>
+	<li>Flowbite Svelte</li>
+	<li>Shadcn Svelte</li>
+</ul>
