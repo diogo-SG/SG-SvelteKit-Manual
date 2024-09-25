16edadda5b8b52e04e4faa5ce85dc7b6f09ef46b
Joao Nacho <joao.nacho@studiographene.com>
2024-09-25 17:34:08 +0100

    advantages of svelte

---
+++ src/routes/extra-info/constants.ts
@@ -0,0 +1,59 @@
+export const SVELTE_PROS_CONS_LIST = [
+	{
+		isPro: true,
+		title: 'Easy to Learn',
+		description:
+			'Offers a simpler, intuitive syntax that makes it easier for new developers to get started quickly.'
+	},
+	{
+		isPro: true,
+		title: 'Reactive Framework',
+		description:
+			'Automatically updates the UI when state changes, without needing complex state management.'
+	},
+	{
+		isPro: true,
+		title: 'Less Boilerplate Code',
+		description:
+			'Achieve more with less code, leading to a cleaner, more efficient development experience compared to other frameworks that require more setup and complexity.'
+	},
+	{
+		isPro: true,
+		title: 'Small Bundle Size',
+		description:
+			'Compiles to optimized vanilla JavaScript, leading to fast load times and minimal overhead.'
+	},
+	{
+		isPro: true,
+		title: 'Component-Based',
+		description:
+			'Simplifies building complex UIs by breaking them into reusable components.'
+	},
+	{
+		isPro: true,
+		title: 'Built-in Transitions',
+		description:
+			'Includes animation and transition support out of the box for smoother user experiences.'
+	},
+	{
+		isPro: true,
+		title: 'SvelteKit Full-Stack Features',
+		description:
+			'Provides routing, server-side rendering (SSR), and API handling in one framework.'
+	},
+	{
+		title: 'Smaller Ecosystem',
+		description:
+			'Fewer libraries and third-party tools compared to more established frameworks like React or Vue.'
+	},
+	{
+		title: 'Community Support',
+		description:
+			'A smaller community means fewer resources or answers for more complex issues.'
+	},
+	{
+		title: 'Limited IDE Support',
+		description:
+			"Some tools may not fully support Svelte's unique syntax, leading to less robust editor features."
+	}
+];
